26a63edeeb8e6b8bbb7a1feb6f32dea48591d049
Karim Manaouil <k.manaouil@gmail.com>
2023-02-28 11:18:43 +0000

    run: add pmbench

--- run.sh
+++ run.sh
@@ -14,7 +14,9 @@ elif [ "$1" = "plot" ]; then
 	elif [ "$2" = "numastat" ]; then
 		python3 process_numastat.py --plot $3
 	elif [ "$2" = "cpustat" ]; then
-		python3 process_cpustat.py --plot
+		python3 process_cpustat.py --plot	
+	elif [ "$2" = "pmbench" ]; then
+		python3 pmbench_plot.py pmbench_access_latency
 	fi
 
 elif [ "$1" = "ima" ]; then
@@ -42,7 +44,8 @@ elif [ "$1" = "pmbench" ]; then
 	pid2=$!
 	python3 process_cpustat.py --run &
 	pid3=$!
-	perf stat numactl -m 0 --cpunodebind 0 ~/pmbench/pmbench -i -j4 -m 8192 -s 8192 -p uniform -r 80 30
+	# numactl -m 2 --cpunodebind 0 perf stat ~/pmbench/pmbench -i -j4 -m 8192 -s 8192 -p uniform -r 80 30
+	perf stat ~/pmbench/pmbench -i -j 4 -m 8192 -s 8192 -p normal --shape=16384 -r 80 30
 	kill -2 $pid1
 	kill -2 $pid2
 	kill -2 $pid3
